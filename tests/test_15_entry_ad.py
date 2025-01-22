061a2619f1c298420fc1dc97dd8d42cd124deb5e
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-01-22 07:31:38 +0100

    test 15 flakiness fix

--- tests/test_15_entry_ad.py
+++ tests/test_15_entry_ad.py
@@ -1,3 +1,5 @@
+import pytest
+
 from utils.create_driver import create_chrome_driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -13,11 +15,15 @@ class TestEntryAd:
 		self.driver.quit()
 
 	def test_entry_ad(self):
-		WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//p[text()="Close"]'))).click()
+		# JS click is needed, because WebElement.click() leads to ElementClickInterceptedException sometimes
+		# element click intercepted: Element <p>...</p> is not clickable at point (949, 592). Other element would receive the click: <div class="modal">...</div>
+		p_close = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//p[text()="Close"]')))
+		self.driver.execute_script('arguments[0].click();', p_close)
+
 		WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.ID, 'modal')))
 		self.driver.refresh()
 		time.sleep(3)
 		WebDriverWait(self.driver, 2).until(EC.invisibility_of_element_located((By.ID, 'modal')))
 		WebDriverWait(self.driver, 2).until(EC.element_to_be_clickable((By.ID, 'restart-ad'))).click()
-		WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, 'modal')))
+		WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'modal')))
 
