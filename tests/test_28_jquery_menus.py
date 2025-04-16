62508d736e68fa0d5cf7acf343ae92b844a0263f
madorjan <madorjan@gmail.com>
2025-04-16 17:49:31 +0200

    restructure download folder clearance for test 28

--- tests/test_28_jquery_menus.py
+++ tests/test_28_jquery_menus.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 
 from utils.base_test import BaseTest
 
@@ -9,16 +8,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Keys
 
 
-def create_download_directory():
-	this_folder_path = os.path.abspath(
-		os.path.join(os.path.realpath(__file__), os.pardir)
-	)
-	path = os.path.join(this_folder_path, 'downloads')
-	return path
-
 class TestInputs(BaseTest):
 	page_url = '/jqueryui/menu'
 
-	def setup_method(self):
-		super().setup_method()
-		create_download_directory()
+	def test_download_pdf(self):
+		assert True
--- utils/base_test.py
+++ utils/base_test.py
@@ -1,8 +1,21 @@
+import os
+
 from utils.create_driver import create_chrome_driver
 
+def clear_download_directory():
+	this_folder_path = os.path.abspath(
+		os.path.join(os.path.realpath(__file__), os.pardir)
+	)
+	path = os.path.join(this_folder_path, '..', 'tests', 'downloads')
+	for filename in os.listdir(path):
+		file_path = os.path.join(path, filename)
+		os.remove(file_path)
 
 class BaseTest(object):
 	page_url = None
+
+	def setup_class(self):
+		clear_download_directory()
 
 	def setup_method(self):
 		self.driver = create_chrome_driver()
