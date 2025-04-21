70b912094941e44f048dbe567edb967d4142c7a0
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-04-21 15:59:38 +0200

    pdate requirements.txt to use compatible release clauses (~=) for dependencies and enhance file download tests in tests/test_28_jquery_menus.py

--- tests/test_28_jquery_menus.py
+++ tests/test_28_jquery_menus.py
@@ -17,20 +17,23 @@ def is_file_present_in_downloads(filename):
 	return os.path.exists(path)
 
 
-def is_file_downloaded(filename):
-	n_of_tries = 0
-	while not is_file_present_in_downloads(filename) and n_of_tries < 50:
-		n_of_tries += 1
-		time.sleep(0.1)
-	return is_file_present_in_downloads(filename)
+def is_file_downloaded(filename, timeout=5, poll_frequency=0.1):
+	start_time = time.monotonic()
+	while time.monotonic() < start_time + timeout:
+		if is_file_present_in_downloads(filename):
+			return True
+		time.sleep(poll_frequency)
+	return False
 
 
 class TestJQueryMenus(BaseTest):
 	page_url = '/jqueryui/menu'
 
 	# we get ElementClickIntercepted if we try to click regularly, therefore Javascript clicking is necessary
-	def click_jquery_element_by_id(self, eid):
-		button = self.wait.until(EC.element_to_be_clickable((By.ID, eid)))
+	def click_jquery_element_by_id(self, element_id):
+		button = self.wait.until(
+			EC.element_to_be_clickable((By.ID, element_id))
+		)
 		self.driver.execute_script('arguments[0].click();', button)
 
 	# in the JQuery menu, the button is considered enabled by Selenium even when disabled
@@ -44,26 +47,16 @@ class TestJQueryMenus(BaseTest):
 			'class'
 		)
 
-	@pytest.mark.xdist_group(name='sequential')
-	def test_download_pdf(self):
+	@pytest.mark.parametrize('file_type, element_id', [
+            ('menu.pdf', 'ui-id-6'),
+            ('menu.csv', 'ui-id-7'),
+            ('menu.xls', 'ui-id-8'),
+        ])
+	def test_download_file(self, file_type, element_id):
 		self.click_jquery_element_by_id('ui-id-2')
 		self.click_jquery_element_by_id('ui-id-4')
-		self.click_jquery_element_by_id('ui-id-6')
-		assert is_file_downloaded('menu.pdf')
-
-	@pytest.mark.xdist_group(name='sequential')
-	def test_download_csv(self):
-		self.click_jquery_element_by_id('ui-id-2')
-		self.click_jquery_element_by_id('ui-id-4')
-		self.click_jquery_element_by_id('ui-id-7')
-		assert is_file_downloaded('menu.csv')
-
-	@pytest.mark.xdist_group(name='sequential')
-	def test_download_excel(self):
-		self.click_jquery_element_by_id('ui-id-2')
-		self.click_jquery_element_by_id('ui-id-4')
-		self.click_jquery_element_by_id('ui-id-8')
-		assert is_file_downloaded('menu.xls')
+		self.click_jquery_element_by_id(element_id)
+		assert is_file_downloaded(file_type)
 
 	def test_back_to_jquery(self):
 		self.click_jquery_element_by_id('ui-id-2')
