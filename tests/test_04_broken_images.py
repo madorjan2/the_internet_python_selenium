daffb2ffb77e791693108183547ab4f8dcb71691
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-04-27 09:51:50 +0200

    fixed test 4 flakyness + updated requirements.txt

--- tests/test_04_broken_images.py
+++ tests/test_04_broken_images.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 
 from utils.base_test import BaseTest
@@ -8,10 +9,18 @@ from selenium.webdriver.common.by import By
 class TestBrokenImages(BaseTest):
 	page_url = '/broken_images'
 
-	def get_nth_image(self, index):
-		return self.driver.find_elements(
-			By.XPATH, '//div[@id="content"]//img'
-		)[index]
+	def get_nth_image(self, index, timeout=5):
+		loaded = False
+		start_time = time.monotonic()
+		imgs = []
+		while not loaded and time.monotonic() - start_time < timeout:
+			imgs = self.driver.find_elements(
+				By.XPATH, '//div[@id="content"]//img'
+			)
+			loaded = len(imgs) >= index + 1
+		if not loaded:
+			pytest.fail(f'Image not loaded in time with index {index}')
+		return imgs[index]
 
 	def test_first_image_is_broken(self):
 		img = self.get_nth_image(0)
@@ -29,8 +38,6 @@ class TestBrokenImages(BaseTest):
 		)
 		assert not loaded
 
-	# ToDo: Flaky, sometimes the third image does not load with the first two
-	@pytest.mark.flaky(reruns=3)
 	def test_third_image_is_not_broken(self):
 		img = self.get_nth_image(2)
 		loaded = self.driver.execute_script(
